c6757ec10dccdf4a457da16632fe8e0432dbe6ce
Alfred Wong <wongab2@mcmaster.ca>
2017-03-04 17:15:37 -0500

    added checking out implementation, still needs error checking

--- src/libcatalog/gui/CheckOutComposite.java
+++ src/libcatalog/gui/CheckOutComposite.java
@@ -1,11 +1,20 @@
 package libcatalog.gui;
 
+import java.io.File;
 import java.util.LinkedList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.wb.swt.SWTResourceManager;
-
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
 import libcatalog.entities.Book;
 
 import org.eclipse.swt.widgets.Label;
@@ -21,7 +30,9 @@ public class CheckOutComposite extends Composite implements MainCompInterface {
 	private Label lblCustomerId;
 	private Label lblBookId;
 	private Button btnCheckOutBook;
-	private LinkedList<Book> listOfBooks;
+	final int REF_NUM_OF_BORROWER = 8;
+	final String BOOK_XMLPATH = "src\\xmlresources\\books.xml";
+
 
 	/**
 	 * Create the composite.
@@ -31,11 +42,10 @@ public class CheckOutComposite extends Composite implements MainCompInterface {
 	 */
 	public CheckOutComposite(Composite parent, int style, LinkedList<Book> listOfBooks) {
 		super(parent, style);
-		this.listOfBooks = listOfBooks;
 		this.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
 		this.setBounds(148, 5, 276, 251);
 		setupWidgets();		
-		
+
 	}
 
 	@Override
@@ -49,31 +59,55 @@ public class CheckOutComposite extends Composite implements MainCompInterface {
 		lblCheckOutBook.setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
 		lblCheckOutBook.setBounds(69, 10, 102, 15);
 		lblCheckOutBook.setText("Check Out a Book");
-		
+
 		lblCustomerId = new Label(this, SWT.NONE);
 		lblCustomerId.setBounds(10, 34, 82, 15);
 		lblCustomerId.setText("Customer ID:");
-		
+
 		textCustomerInput = new Text(this, SWT.BORDER);
-		textCustomerInput.setVisible(false);
 		textCustomerInput.setBounds(107, 31, 117, 21);
-		
+
 		lblBookId = new Label(this, SWT.NONE);
 		lblBookId.setBounds(10, 69, 55, 15);
 		lblBookId.setText("Book ID:");
-		
+
 		textBookInput = new Text(this, SWT.BORDER);
 		textBookInput.setBounds(107, 63, 117, 21);
-		
+
 		btnCheckOutBook = new Button(this, SWT.NONE);
 		btnCheckOutBook.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
-				
+				writeCustomerToBook();				
 			}
 		});
 		btnCheckOutBook.setBounds(47, 103, 128, 25);
 		btnCheckOutBook.setText("Check Out Book");
+
+	}
+
+	protected void writeCustomerToBook() {
+		DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder documentBuilder;
+		/* parse existing file to DOM */ 
+		Document document;
+		try {
+			documentBuilder = documentBuilderFactory.newDocumentBuilder();
+			document = documentBuilder.parse(new File(BOOK_XMLPATH));
+			Node bookToCheckOut = document.getElementsByTagName("book").item(Integer.parseInt(textBookInput.getText())-1);
+			Node borrowerAttr = bookToCheckOut.getChildNodes().item(REF_NUM_OF_BORROWER);
+			borrowerAttr.setNodeValue(textCustomerInput.getText());
+			// Write to XML file
+			TransformerFactory transformerFactory = TransformerFactory.newInstance();
+			Transformer transformer = transformerFactory.newTransformer();
+			DOMSource source = new DOMSource(document);
+			StreamResult result = new StreamResult(new File(BOOK_XMLPATH));
+			transformer.transform(source, result);
+
+		}catch (Exception e) {
+			e.printStackTrace();
+			// TODO: handle exception
+		}
 
 	}
 }
